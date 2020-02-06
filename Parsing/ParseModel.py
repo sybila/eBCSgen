c8f655ead79aed4b7ad73f3e876fdc67d6ee11d3
xtrojak <trojak@mail.muni.cz>
2020-02-06 15:24:17 +0100

    Parser basically finished

--- Parsing/ParseModel.py
+++ Parsing/ParseModel.py
@@ -1,7 +1,24 @@
+import collections
+from copy import deepcopy
 from lark import Lark, Transformer, v_args
 
 from Objects.Atomic import AtomicAgent
+from Objects.Complex import Complex
+from Objects.Rule import Rule
 from Objects.Structure import StructureAgent
+
+class SideHelper:
+    def __init__(self):
+        self.seq = []
+        self.comp = []
+        self.complexes = []
+        self.counter = 0
+
+    def __str__(self):
+        return " | ".join([str(self.seq), str(self.comp), str(self.complexes), str(self.counter)])
+
+    def __repr__(self):
+        return str(self)
 
 grammar = r"""
     start: rule
@@ -15,8 +32,10 @@ grammar = r"""
     composition: (atomic ",")* atomic?
     atomic : a_name "{" state "}" | a_name
 
-    rate : fun "/" fun | fun
-    fun: const | param | "[" complex "]" | fun "+" fun | fun "*" fun | fun "^" const
+    !rate : fun "/" fun | fun
+    !fun: const | param | "[" rate_complex "]" | fun "+" fun | fun "*" fun | fun "^" const
+
+    rate_complex: sequence "::" compartment
 
     state: CNAME | "_"
     a_name: CNAME
@@ -50,9 +69,47 @@ class TreeToObjects(Transformer):
         else:
             return StructureAgent(name, set())
 
+    def rate_complex(self, matches):
+        sequence = []
+        for item in matches[0].children:
+            sequence.append(item.children[0])
+        compartment = matches[1]
+        return Complex(collections.Counter(sequence), compartment)
 
-parser = Lark(grammar,
-              parser='lalr',
+    def compartment(self, matches):
+        return str(matches[0])
+
+    def const(self, matches):
+        return int(matches[0])
+
+    def side(self, matches):
+        helper = SideHelper()
+        stochio = 1
+        for item in matches:
+            if type(item) == int:
+                stochio = item
+            else:
+                agents = item.children[0]
+                compartment = item.children[1]
+                for i in range(stochio):
+                    start = helper.counter
+                    for agent in agents.children:
+                        helper.seq.append(agent.children[0])
+                        helper.comp.append(compartment)
+                        helper.counter += 1
+                    helper.complexes.append((start, helper.counter - 1))
+        return helper
+
+    def rule(self, matches):
+        lhs, rhs, rate = matches
+        agents = tuple(lhs.seq + rhs.seq)
+        mid = lhs.counter
+        compartments = lhs.comp + rhs.comp
+        complexes = lhs.complexes + list(map(lambda item: (item[0] + lhs.counter, item[1] + lhs.counter), rhs.complexes))
+        pairs = [(i, i +lhs.counter) for i in range(lhs.counter - 1)]
+        return Rule(agents, mid, compartments, complexes, pairs, rate)
+
+parser = Lark(grammar, parser='lalr',
               lexer='standard',
               propagate_positions=False,
               maybe_placeholders=False,
@@ -62,11 +119,11 @@ parser = Lark(grammar,
 
 def test():
     test_atomic = '''
-        1 K(S{u3_m}, T{a_p}, S{p})::cyt + 1 B()::cyt => 1 B().K(S{p}).H()::cyt + 2 A{_}::cyt @ 3*[K()::cyt]/2*v_1
+        1 K(S{u3_m}, T{a_p}, S{p})::cyt + B()::cyt => 1 B().K(S{p}).H()::cyt + 2 A{_}::cyt @ 3*[K()::cyt]/2*v_1
     '''
 
     j = parser.parse(test_atomic)
-    print(j.pretty())
+    print(j.children[0])
 
 
 if __name__ == '__main__':
