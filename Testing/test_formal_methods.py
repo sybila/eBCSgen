a6ba7bb837df3b5d4c4fe3c454bd966d840e79b5
xmertova <lmertova12@gmail.com>
2020-04-08 14:35:05 +0200

    add test formal methods

--- Testing/test_formal_methods.py
+++ Testing/test_formal_methods.py
@@ -1,142 +1,205 @@
-import subprocess
 import unittest
 
+from matplotlib import collections
 from sympy.printing.tests.test_numpy import np
 
 from Core.Model import Model
-from Core.Complex import Complex
-from Core.Structure import StructureAgent
+from Parsing.ParsePCTLformula import PCTLparser
 from TS.Edge import Edge
 from TS.State import State
 from TS.TransitionSystem import TransitionSystem
+from Core.Structure import StructureAgent
+from Core.Complex import Complex
+from Core.Rule import Rule
+from Parsing.ParseBCSL import Parser
+from TS.State import State
+import re
 
 
 class TestFormalMethods(unittest.TestCase):
     def setUp(self):
-        """
-        Model 1
-        Analysis of a PRISM model of the Knuth-Yao die
-        storm website
-        """
-        self.str1 = StructureAgent("S", set())
-        self.str2 = StructureAgent("D", set())
+        self.parser = PCTLparser()
+        self.model_parser = Parser("model")
+        self.model = """
+            #! rules
+            Y{i}::rep => Y{a}::rep @ p*[Y{i}::rep]
+            Y{i}::rep => Y{-}::rep @ (1-p)*[Y{i}::rep]
+            X()::rep + Y{a}::rep => X().Y{a}::rep @ q*[X()::rep]*[Y{a}::rep]
+            X(K{i}).Y{_}::rep => X(K{p}).Y{_}::rep @ p*[X(K{i}).Y{_}::rep] // also here
 
-        self.c1 = Complex([self.str1], "rep")
-        self.c2 = Complex([self.str2], "rep")
+            #! inits
+            2 X(K{i})::rep
+            1 Y{i}::rep
 
-        ordering = (self.c1, self.c2)
-
-        self.s1 = State(np.array((0, 0)))
-        self.s2 = State(np.array((1, 0)))
-        self.s3 = State(np.array((2, 0)))
-        self.s4 = State(np.array((3, 0)))
-        self.s5 = State(np.array((4, 0)))
-        self.s6 = State(np.array((5, 0)))
-        self.s7 = State(np.array((6, 0)))
-        self.s8 = State(np.array((7, 1)))
-        self.s9 = State(np.array((7, 2)))
-        self.s10 = State(np.array((7, 3)))
-        self.s11 = State(np.array((7, 4)))
-        self.s12 = State(np.array((7, 5)))
-        self.s13 = State(np.array((7, 6)))
-
-        self.die_ts = self.ts_eq_1 = TransitionSystem(ordering)
-        self.ts_eq_1.states_encoding = {self.s1: 0, self.s2: 1, self.s3: 2, self.s4: 3, self.s5: 4,
-                                        self.s6: 5, self.s7: 6, self.s8: 7, self.s9: 8, self.s10: 9,
-                                        self.s11: 10, self.s12: 11, self.s13: 12}
-        self.die_ts.edges = {Edge(0, 1, 0.5), Edge(0, 2, 0.5), Edge(1, 2, 0.5), Edge(1, 3, 0.5), Edge(2, 4, 0.5),
-                             Edge(2, 5, 0.5), Edge(3, 1, 0.5), Edge(3, 7, 0.5), Edge(4, 8, 0.5), Edge(4, 9, 0.5),
-                             Edge(5, 10, 0.5), Edge(5, 11, 0.5), Edge(6, 2, 0.5), Edge(6, 12, 0.5), Edge(7, 7, 1),
-                             Edge(8, 8, 1), Edge(9, 9, 1), Edge(10, 10, 1), Edge(11, 11, 1), Edge(12, 12, 1)}
-
-        # TODO -> model definition
-        # self.defs = {}
-        # self.inits = collections.Counter({self.c1: 0, self.c2: 0})
-        # self.die_model = self.model = Model({}, self.inits, self.defs, None)
-
-        self.die_prism = """
-dtmc
-
-module die
-
-    // local state
-    s : [0..7] init 0;
-    // value of the dice
-    d : [0..6] init 0;
-
-    [] s=0 -> 0.5 : (s'=1) + 0.5 : (s'=2);
-    [] s=1 -> 0.5 : (s'=3) + 0.5 : (s'=4);
-    [] s=2 -> 0.5 : (s'=5) + 0.5 : (s'=6);
-    [] s=3 -> 0.5 : (s'=1) + 0.5 : (s'=7) & (d'=1);
-    [] s=4 -> 0.5 : (s'=7) & (d'=2) + 0.5 : (s'=7) & (d'=3);
-    [] s=5 -> 0.5 : (s'=7) & (d'=4) + 0.5 : (s'=7) & (d'=5);
-    [] s=6 -> 0.5 : (s'=2) + 0.5 : (s'=7) & (d'=6);
-    [] s=7 -> (s'=7);
-
-endmodule
-
-rewards "coin_flips"
-    [] s<7 : 1;
-endrewards
-        """
-        self.die_pctl1 = "P=? [F \"one\"]"
-        self.die_pctl2 = "P=? [F s=7&d=1]"
-        self.die_transitions = """dtmc
-0 1 0.5
-0 2 0.5
-1 3 0.5
-1 4 0.5
-2 5 0.5
-2 6 0.5
-3 1 0.5
-3 7 0.5
-4 8 0.5
-4 9 0.5
-5 10 0.5
-5 11 0.5
-6 2 0.5
-6 12 0.5
-7 7 1
-8 8 1
-9 9 1
-10 10 1
-11 11 1
-12 12 1
+            #! definitions
+            p = 0.3
         """
 
-    # TODO missing test for label file, ordering of target state does not correspond
-    def test_explicit_file(self):
-        self.die_ts.init = 6
-        labels = {6: {'property_0', 'init'}, 11: {'property_0', 'property_1'},
-                  3: {'property_3', 'property_0'}, 8: {'property_1'}}
-        APs = {'smth0': 'property_0', 'smth1': 'property_1', 'smth2': 'property_2', 'smth3': 'property_3', }
-        self.die_ts.save_to_STORM_explicit("explicit_transitions.tra", "explicit_labels.lab", labels, APs)
-        with open('explicit_transitions.tra', 'r') as f:
-            print(f.read())
-        with open('explicit_labels.lab', 'r') as f:
-            print(f.read())
-        # self.assertEqual(open("explicit_transitions.tra", "r").read(), self.die_transitions)
+        self.tumor = """
+            #! rules
+            T(P{i})::x => T(P{m})::x @ a1*[T(P{i})::x]
+            T(P{m})::x => T(P{i})::x + T(P{i})::x @ a2*[T(P{m})::x]
+            T(P{i})::x => @ d1*[T(P{i})::x]
+            T(P{m})::x => @ d2*[T(P{m})::x]
 
-    # # TODO bcsl model for die example labeling file
-    # def test_model_checking(self):
-    #     # model_checking1 = self.die_model.PCTL_model_checking(self.die_pctl1)
-    #     out = subprocess.Popen(
-    #         ['storm', '--explicit', 'die.tra', 'die.lab', '--prop', self.die_pctl1],
-    #         stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    #     stdout, stderr = out.communicate()
-    #     # self.assertEqual(model_checking1, stdout)
+            #! inits
+            2 T(P{m})::x
+            1 T(P{i})::x
 
-    # TODO
-    def test_prism_file(self):
-        # bound does not correspond to model 1
-        self.die_ts.init = 6
-        self.die_ts.save_to_prism("prism_dtmc.pm", 7, {"p", "q"}, ['ABSTRACT_VAR_12 = VAR_1+VAR_2',
-                                                                   'ABSTRACT_VAR_34 = VAR_3+VAR_4'])
-        with open('prism_dtmc.pm', 'r') as f:
-            print(f.read())
+            #! definitions
+            a1 = 1.2
+            a2 = 2
+            d1 = 0.8
+            d2 = 0.5
+        """
 
-        # self.assertEqual(open("prism_dtmc.pm", "r").read(), self.die_prism)
+        self.tumor_parametric = """
+            #! rules
+            T(P{i})::x => T(P{m})::x @ a1*[T(P{i})::x]
+            T(P{m})::x => T(P{i})::x + T(P{i})::x @ a2*[T(P{m})::x]
+            T(P{i})::x => @ d1*[T(P{i})::x]
+            T(P{m})::x => @ d2*[T(P{m})::x]
 
-    # TODO
+            #! inits
+            2 T(P{m})::x
+            1 T(P{i})::x
+
+            #! definitions
+            a1 = 1.2
+            a2 = 2
+            d1 = 0.8
+        """
+
+    # test second model --> tumor
+    def test_tumor_modelchecking_1(self):
+        model_parsed = self.model_parser.parse(self.tumor).data
+        ts = model_parsed.to
+        result = model_parsed.PCTL_model_checking("P=? [F T(P{m})::x>2 & T(P{i})::x>=0 & T(P{i})::x<2]")
+        print("model checking 1", result)
+        self.assertTrue("Result (initial states)" in str(result))
+
+    def test_tumor_modelchecking_2(self):
+        model_parsed = self.model_parser.parse(self.tumor).data
+        result = model_parsed.PCTL_model_checking("P > 0.5 [F T(P{m})::x>2]")
+        self.assertTrue("Result (initial states)" in str(result))
+
+    def test_tumor_modelchecking_wrong_formula(self):
+        model_parsed = self.model_parser.parse(self.tumor).data
+        result = model_parsed.PCTL_model_checking("P > 0.5 [F T(P{m}):x>2]")
+        error_message = {'line': 1, 'column': 19, 'unexpected': ':', 'expected': {'DOUBLE_COLON', '.'}}
+        self.assertEqual(result, error_message)
+
     def test_parameter_synthesis(self):
-        self.assertEqual("", "")
+        model_parsed = self.model_parser.parse(self.tumor_parametric).data
+        result = model_parsed.PCTL_synthesis("P=? [F T(P{m})::x>2]", "0<=d2<=2")
+        print("param_synthesis_tumor: ", result)
+        self.assertTrue("Result" in str(result))
+
+    """    def test_parametric_model(self):
+        model_parsed = self.model_parser.parse(self.model).data
+        ts = model_parsed.to_vector_model().generate_transition_system()
+        ts.save_to_prism("prism-parametric.pm", 2, {"q"}, [])
+        out = model_parsed.PCTL_synthesis("P=<0.3 (F [X().Y{_}::rep => 1])", "")
+        self.assertTrue("Result (for initial states)" in str(out))"""
+
+    def test_synthesis_simple(self):
+        model_str = """
+        #! rules
+        X()::rep => @ k1*[X()::rep]
+        Z()::rep => X()::rep @ k2
+        => Y()::rep @ 1/(1+([X()::rep])**4)
+
+        #! inits
+        2 X()::rep
+        Y()::rep
+
+        #! definitions
+        k2 = 5
+        """
+        model_parser = Parser("model")
+        model = model_parser.parse(model_str).data
+        formula = 'P <= 0.5[F X()::rep = 1]'
+        region = '0<=k1<=1'
+        output = model.PCTL_synthesis(formula, region)
+        self.assertTrue("Region results" in str(output))
+
+        formula = 'P=? [F X()::rep = 1]'
+        output = model.PCTL_synthesis(formula, None)
+        self.assertTrue("Result (initial states)" in str(output))
+
+    def test_synthesis_advanced(self):
+        model_str = """
+        #! rules
+        // commenting
+        X(K{i})::rep => X(K{p})::rep @ k1*[X()::rep] // also here
+        X(T{a})::rep => X(T{o})::rep @ k2*[Z()::rep]
+        => Y(P{f})::rep @ 1/(1+([X()::rep])**4) // ** means power (^)
+
+        #! inits
+        2 X(K{c}, T{e}).X(K{c}, T{j})::rep
+        Y(P{g}, N{l})::rep
+
+        #! definitions
+        k2 = 0.05 // also comment
+        """
+        model_parser = Parser("model")
+        model = model_parser.parse(model_str).data
+        formula = 'P <= 0.5[F X()::rep = 1]'
+        region = '0<=k1<=1'
+        output = model.PCTL_synthesis(formula, region)
+        self.assertTrue("Region results" in str(output))
+
+        formula = 'P=? [F X()::rep = 1]'
+        output = model.PCTL_synthesis(formula, None)
+        self.assertTrue("Result (initial states)" in str(output))
+
+    def test_model_checking_simple(self):
+        model_str = """
+        #! rules
+        X()::rep => @ k1*[X()::rep]
+        Z()::rep => X()::rep @ k2
+        => Y()::rep @ 1/(1+([X()::rep])**4)
+
+        #! inits
+        2 X()::rep
+        Y()::rep
+
+        #! definitions
+        k2 = 5
+        k1 = 2
+        """
+        model_parser = Parser("model")
+        model = model_parser.parse(model_str).data
+        formula = 'P <= 0.5[F X()::rep=1]'
+        output = model.PCTL_model_checking(formula)
+        self.assertTrue("Result (for initial states)" in str(output))
+
+        formula = 'P=? [F X()::rep = 1]'
+        output = model.PCTL_model_checking(formula)
+        self.assertTrue("Result (for initial states)" in str(output))
+
+    def test_model_checking_advanced(self):
+        model_str = """
+        #! rules
+        // commenting
+        X(K{i})::rep => X(K{p})::rep @ k1*[X()::rep] // also here
+        X(T{a})::rep => X(T{o})::rep @ k2*[Z()::rep]
+        => Y(P{f})::rep @ 1/(1+([X()::rep])**4) // ** means power (^)
+
+        #! inits
+        2 X(K{c}, T{e}).X(K{c}, T{j})::rep
+        Y(P{g}, N{l})::rep
+
+        #! definitions
+        k2 = 0.05 // also comment
+        k1 = 2
+        """
+        model_parser = Parser("model")
+        model = model_parser.parse(model_str).data
+        formula = 'P >= 0.5[F X()::rep=1]'
+        output = model.PCTL_model_checking(formula)
+        self.assertTrue("Result (for initial states)" in str(output))
+
+        formula = 'P=? [F X()::rep = 1]'
+        output = model.PCTL_model_checking(formula)
+        self.assertTrue("Result (for initial states)" in str(output))
