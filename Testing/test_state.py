28899bfcd00a17c9653b32b6105e8c010904c31f
xtrojak <trojak@mail.muni.cz>
2021-09-24 10:24:15 +0200

    Fixed tests for state

--- Testing/test_state.py
+++ Testing/test_state.py
@@ -3,18 +3,23 @@ import numpy as np
 
 import Parsing.ParseBCSL
 from Core.Formula import AtomicProposition
-from TS.State import VectorState
+from TS.State import State, Memory, Vector
 
 
 class TestState(unittest.TestCase):
     def setUp(self):
-        self.s1 = VectorState(np.array((1, 2, 3)))
-        self.s2 = VectorState(np.array((5, 4, 3)))
-        self.s3 = VectorState(np.array((5, 4, 3, 2)))
-        self.s4 = VectorState(np.array((2, 2, 2, 1)))
-        self.s5 = VectorState(np.array((7, 6, 5, 3)))
-        self.s6 = VectorState(np.array((1, 0, 0, 1, 0)))
-        self.s_inf = VectorState(np.array((np.inf, np.inf, np.inf)))
+        self.s1 = State(Vector(np.array((1, 2, 3))), Memory(0))
+        self.s2 = State(Vector(np.array((5, 4, 3))), Memory(0))
+        self.s3 = State(Vector(np.array((5, 4, 3, 2))), Memory(0))
+        self.s4 = State(Vector(np.array((2, 2, 2, 1))), Memory(0))
+        self.s5 = State(Vector(np.array((7, 6, 5, 3))), Memory(0))
+        self.s6 = State(Vector(np.array((1, 0, 0, 1, 0))), Memory(0))
+        self.s_inf = State(Vector(np.array((np.inf, np.inf, np.inf))), Memory(0), True)
+
+        self.consumed_1 = Vector(np.array((0, 0, 0)))
+        self.consumed_2 = Vector(np.array((0, 0, 0, 0)))
+        self.produced_1 = Vector(np.array((5, 4, 3)))
+        self.produced_2 = Vector(np.array((2, 2, 2, 1)))
 
         complex_parser = Parsing.ParseBCSL.Parser("rate_complex")
 
@@ -22,28 +27,53 @@ class TestState(unittest.TestCase):
         self.complex_2 = complex_parser.parse("K(S{a},T{a}).B{o}::cyt").data.children[0]
         self.complex_3 = complex_parser.parse("K(S{a},T{i}).B{o}::cyt").data.children[0]
 
+    def test_memory(self):
+        # no memory
+        mem = Memory(0)
+        mem.update_memory('test')
+        mem_expected = Memory(0)
+        self.assertEqual(mem, mem_expected)
+
+        # one step memory
+
+        mem = Memory(1)
+        mem.update_memory('test')
+        mem_expected = Memory(1)
+        mem_expected.history = ['test']
+        self.assertEqual(mem, mem_expected)
+
+        mem.update_memory('next')
+        mem_expected = Memory(1)
+        mem_expected.history = ['next']
+        self.assertEqual(mem, mem_expected)
+
+        # infinite memory
+
+        mem = Memory(2)
+        mem.update_memory('test')
+        mem.update_memory('next')
+        mem_expected = Memory(2)
+        mem_expected.history = ['test', 'next']
+        self.assertEqual(mem, mem_expected)
+
     def test_sub(self):
-        self.assertEqual(self.s1 - self.s2, VectorState(np.array((-4, -2, 0))))
-        self.assertEqual(self.s3 - self.s4, VectorState(np.array((3, 2, 1, 1))))
+        self.assertEqual(self.s1 - self.s2, State(Vector(np.array((-4, -2, 0))), Memory(0)))
+        self.assertEqual(self.s3 - self.s4, State(Vector(np.array((3, 2, 1, 1))), Memory(0)))
         with self.assertRaises(ValueError):
             self.s2 - self.s3
 
-    def test_check_negative(self):
-        self.assertFalse((self.s1 - self.s2).check_negative())
-        self.assertTrue((self.s3 - self.s4).check_negative())
-
     def test_add_with_bound(self):
         bound = 5
-        self.assertEqual(self.s1.add_with_bound(self.s2, bound), self.s_inf)
+        self.assertEqual(self.s1.update_state(self.consumed_1, self.produced_1, 'label', bound), self.s_inf)
         bound = 8
-        self.assertEqual(self.s3.add_with_bound(self.s4, bound), self.s5)
+        self.assertEqual(self.s3.update_state(self.consumed_2, self.produced_2, 'label', bound), self.s5)
 
     def test_to_ODE_string(self):
         self.assertEqual(self.s6.to_ODE_string(), "y[0] + y[3]")
 
     def test_reorder(self):
         order = np.array([2, 0, 1])
-        self.assertEqual(self.s1.reorder(order), VectorState(np.array((3, 1, 2))))
+        self.assertEqual(self.s1.reorder(order), State(Vector(np.array((3, 1, 2))), Memory(0)))
 
     def test_check_AP(self):
         ordering = (self.complex_1, self.complex_2, self.complex_3)
