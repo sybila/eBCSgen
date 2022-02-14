8fd4be11a67667a778823782d6a7f15fe2818cd3
Matej Trojak <trojak@mail.muni.cz>
2022-02-14 08:59:58 +0100

    Fixed setup.py file

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='eBCSgen',
-      version='1.1',
+      version='1.3',
       description='eBSCgen is a tool for development and analysis of models written in Biochemical Space Language',
       url='https://github.com/sybila/eBCSgen',
       author='Mate Trojak',
       author_email='xtrojak@fi.muni.cz',
       license='MIT',
-      packages=['eBCSgen'],
+      packages=find_packages(exclude=['*Testing*']),
+      test_suite="Testing",
       zip_safe=False)
