64c5114e691a7d2bf27e4c2c4a1aafe7774cdf55
Matej Trojak <trojak@mail.muni.cz>
2022-03-02 10:30:16 +0100

    Fixed paths in SBML testing

--- Testing/test_export_SBML.py
+++ Testing/test_export_SBML.py
@@ -1,5 +1,6 @@
 import unittest
 import libsbml
+import os
 
 from eBCSgen.Parsing.ParseBCSL import Parser
 
@@ -9,12 +10,15 @@ class TestSBMLexport(unittest.TestCase):
         model_parser = Parser("model")
         self.models_to_test = {}
 
+        if not os.path.exists("Testing/Output/"):
+            os.mkdir("Testing/Output/")
+
         with open("Testing/BCSL_models_for_SBML_export/general.txt", "r") as model_exp:
             self.models_to_test["general"] = model_parser.parse(model_exp.read()).data
         with open("Testing/BCSL_models_for_SBML_export/izomorphic.txt", "r") as model_izo:
-            self.models_to_test["general"] = model_parser.parse(model_izo.read()).data
+            self.models_to_test["izomorphic"] = model_parser.parse(model_izo.read()).data
         with open("Testing/BCSL_models_for_SBML_export/transition.txt", "r") as model_transition:
-            self.models_to_test["general"] = model_parser.parse(model_transition.read()).data
+            self.models_to_test["transition"] = model_parser.parse(model_transition.read()).data
 
     def test_by_validator(self):
         validator = libsbml.SBMLValidator()
@@ -24,4 +28,4 @@ class TestSBMLexport(unittest.TestCase):
             validator.setDocument(document)
             validator.validate()
             self.assertEqual(validator.getNumFailures(), 0)
-            libsbml.writeSBMLToFile(document, "../Export/Output/"+model+".xml")
+            libsbml.writeSBMLToFile(document, "Testing/Output/"+model+".xml")
