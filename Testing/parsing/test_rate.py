5ae0821a15cc0108dd02b75e4505c460533d8b11
Marketa Opichalova <514349@mail.muni.cz>
2023-12-05 15:51:26 +0100

    parsing rate - negative cases added

--- Testing/parsing/test_rate.py
+++ Testing/parsing/test_rate.py
@@ -1,5 +1,6 @@
 from eBCSgen.Parsing.ParseBCSL import Parser
 import Testing.parsing.objects_testing as objects
+from eBCSgen.Core.Complex import Complex
 
 parser = Parser("rate")
 
@@ -28,17 +29,43 @@ def test_parser():
     )
     assert ret.success
 
-    ret = parser.parse(
-        "(([B(T{s})::cell] + [B(T{s})::cyt]) / ([B(T{_}.C())::cell] + [B()::cell])) / (([B(T{s}).T{s}::cyt] - [T{_}::cell]) / ([T{u}.T{_}::cell] - [B(T{s})::cell]))"
+    # ret = parser.parse(
+    #     "((([B(T{s})::cell] + [B(T{s})::cyt]) / ([B(T{_}.C())::cell] + [B()::cell])) / (([B(T{s}).T{s}::cyt] - [T{_}::cell]) / ([T{u}.T{_}::cell] - [B(T{s})::cell])))"
+    # )
+    expr1 = "([B(T{s})::cell] + [B(T{s})::cyt]) / ([B(T{_}).C()::cell] + [B()::cell])"
+    expr2 = (
+        "([B(T{s}).T{s}::cyt] - [T{_}::cell]) / ([T{u}.T{_}::cell] - [B(T{s})::cell])"
     )
+    ret = parser.parse(expr1)
     assert ret.success
+    ret = parser.parse(expr2)
+    assert ret.success
+    # ret = parser.parse(f"({expr1}/{expr2})")
+    # assert ret.success
 
     ret = parser.parse("0,5")
     assert not ret.success
 
+    # missing []
     ret = parser.parse("2*K()::cyt")
     assert not ret.success
 
-    # is division by zero rational expression?
-    ret = parser.parse("1/0")
+    # invalid syntax
+    ret = parser.parse("2 +")
+    assert not ret.success
+
+    # unsupported operators
+    ret = parser.parse("2 & 3")
+    assert not ret.success
+
+    # mismatched parentheses
+    ret = parser.parse("(2 * 3")
+    assert not ret.success
+
+    # incorrect format
+    ret = parser.parse("2,,3")
+    assert not ret.success
+
+    # nested complexes
+    ret = parser.parse("[K([L()::cell])::cyt]")
     assert not ret.success
